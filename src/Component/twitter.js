144d03442f57e14a924540207bc3897d35903ebf
anashussain08 <anas.hussain08@gmail.com>
2017-12-05 17:43:05 +0500

    api written

--- src/Component/twitter.js
+++ src/Component/twitter.js
@@ -1,4 +1,5 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import Twitter from 'twitter';
 export default class TwitterApi extends Component{
   constructor(){
     super();
@@ -15,6 +16,10 @@ inputChange(changeValue){
   console.log("Change Value"+this.state.search_text);
  }
   render(){
+    //       twitter.getSearch({'q':'#haiku','count': 10}, error, res).then((res) => {
+    //       console.log("here is the responce"+JSON.stringify(res));
+    //   })
+          
    return(
      <div>
       <input type="text" value={this.state.search_text} onChange={this.inputChange.bind(this)}/>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Twitter from 'twitter';
+
+var client = new Twitter({
+  consumer_key:'yzOGIgdFO0RET6j9rjNio3Cm6',
+  consumer_secret:'2yPkrkt0CIHpbIHUPh9AOqonW4zfGn7BpYsqTkAupQngpiMb1r',
+  access_token_key:'938017226992553984-dRm60aVBgJFFv0HKBKAKEKs8nIw4MW7',
+  access_token_secret: 'yh3t7bzMJxyyGR55frSU4l64Nalgiy2beMNMdftPLZ5RW'
+});
+// var error = function (err, response, body) {
+//         console.log('ERROR [%s]', err);
+//     };
+//     var res = function (data) {
+//         console.log('Data [%s]', data);
+//     };
+//  let twitter = new Twitter(config);
+
+client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
+  if(error) throw error;
+  console.log(tweet);  // Tweet body. 
+  console.log(response);  // Raw response object. 
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
